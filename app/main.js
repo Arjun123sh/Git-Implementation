63c200ccc1795bcb48eb6f6c6dcf08e6823128ab
unknown <arjunrocks405@gmail.com>
2023-12-24 16:19:29 +0530

    First Commit

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const zlib=require("zlib");
 console.log("Logs from your program will appear here!");
 
 
@@ -9,6 +9,9 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    catFile(arguments);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -23,4 +26,13 @@ function createGitDirectory() {
   console.log("Initialized git directory");
 }
 
-
+async function catFile(arguments) {
+  const objectSha = arguments.at(-1);
+  const objectFolder = objectSha[0] + objectSha[1];
+  const objectFile = objectSha.slice(2);
+  const compressedFileContent = fs.readFileSync(path.join(__dirname, '.git', 'objects', objectFolder, objectFile));
+  const uncompressedFileContent = zlib.inflateSync(compressedFileContent);
+  const headerEnd = uncompressedFileContent.toString().indexOf('\x00');
+1
+  process.stdout.write(uncompressedFileContent.toString('utf-8').slice(headerEnd + 1));
+}
